1f8d5ddeddcc9603f763ed0880384c7c1a91e1de
slin1202 <slin1202@gmail.com>
2015-04-14 11:56:39 -0400

    Update barcodescanner.js

--- www/barcodescanner.js
+++ www/barcodescanner.js
@@ -37,26 +37,8 @@
              * @type Object
              */
             this.format = {
-                "all_1D": 61918,
-                "aztec": 1,
-                "codabar": 2,
                 "code_128": 16,
-                "code_39": 4,
-                "code_93": 8,
-                "data_MATRIX": 32,
-                "ean_13": 128,
-                "ean_8": 64,
-                "itf": 256,
-                "maxicode": 512,
-                "msi": 131072,
-                "pdf_417": 1024,
-                "plessey": 262144,
-                "qr_CODE": 2048,
-                "rss_14": 4096,
-                "rss_EXPANDED": 8192,
-                "upc_A": 16384,
-                "upc_E": 32768,
-                "upc_EAN_EXTENSION": 65536
+                "code_39": 4
             };
         };
 
